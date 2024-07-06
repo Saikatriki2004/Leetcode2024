fe8e381c15305e4c7071618b59854dd547d4693b
Saikat Rana <149765443+Saikatriki2004@users.noreply.github.com>
2024-07-06 12:42:09 +0530

    9.Palindrome Number

--- python/9. Palindrome Number.py
+++ python/9. Palindrome Number.py
@@ -1,29 +1,17 @@
-Given an integer x, return true if x is a 
-palindrome
-, and false otherwise.
-
- 
-
-Example 1:
-
-Input: x = 121
-Output: true
-Explanation: 121 reads as 121 from left to right and from right to left.
-Example 2:
-
-Input: x = -121
-Output: false
-Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
-Example 3:
-
-Input: x = 10
-Output: false
-Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
- 
-
-Constraints:
-
--231 <= x <= 231 - 1
- 
-
-Follow up: Could you solve it without converting the integer to a string?
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        # Negative numbers are not palindromes
+        if x < 0:
+            return False
+        
+        # Numbers ending in 0 (except 0 itself) are not palindromes
+        if x % 10 == 0 and x != 0:
+            return False
+        
+        reverted_number = 0
+        while x > reverted_number:
+            reverted_number = reverted_number * 10 + x % 10
+            x //= 10
+        
+        # When the length is an odd number, we can get rid of the middle digit by reverted_number // 10
+        return x == reverted_number or x == reverted_number // 10
