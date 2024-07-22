f57f748125b2fa69743b3a93d45459d6bf5066f4
Saikat Rana <149765443+Saikatriki2004@users.noreply.github.com>
2024-07-22 16:38:10 +0530

    Delete py.py

--- py.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
-
-# Example usage
-sol = Solution()c
-print(sol.strStr("sadbutsad", "sad"))  # Output: 0
-print(sol.strStr("leetcode", "leeto"))  # Output: -1
